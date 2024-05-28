ebc93f9f5941bff5e7c051aa48c195dd8143faa2
jaenfigueroa <jaenfigueroa03042000@gmail.com>
2024-05-28 17:55:01 -0500

    Agregar archivos de proyecto.

---
+++ MiProyecto/Models/Haberes_at.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MiProyecto.Models
+{
+    public partial class Haberes
+    {
+        public string Dni { get; set; }
+        public string Nombre { get; set; }
+        public string Categoria { get; set; }
+        public double SueldoBasico { get; set; }
+        public string TipoAportacion { get; set; }
+
+    }
+}
---
+++ MiProyecto/Models/Haberes_op.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MiProyecto.Models
+{
+    public partial class Haberes
+    {
+        public double Bonificacion() {
+            return SueldoBasico * 0.25;
+        }
+
+        public double Remuneracion() {
+            return SueldoBasico + Bonificacion();
+        }
+
+        public double Aportacion() {
+            if (TipoAportacion == "AFP") {
+                return Remuneracion() * 0.11;
+            }
+            if (TipoAportacion == "ONP") {
+                return Remuneracion() * 0.13;
+            }
+            else {
+                return 0;
+            }
+        }
+
+        public double Neto() {
+            return Remuneracion() - Aportacion();
+        }
+    }
+}
